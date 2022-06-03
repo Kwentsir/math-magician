9ccdca6bafdd4372195d3353b7ed4b5de11fc2c6
Joseph Kwesi Kwentsir <kwentsirkwesi@gmail.com>
2022-06-03 10:38:39 +0000

    calculator logic

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,110 +1,105 @@
 import React, { Component } from 'react';
+import Blocks from './Blocks';
+import calculate from './Calculation';
 
 export default class Calculator extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
   }
+
+  handleCalculate = (label) => {
+    const { total, next, operation } = this.state;
+    const newObj = calculate({ total, next, operation }, label);
+    this.setState({
+      total: newObj.total,
+      operation: newObj.operation,
+      next: newObj.next,
+    });
+  };
 
   render() {
     const calculatorOptions = [
       {
-        label: 0,
-        operation: 'answer',
-      },
-      {
         label: 'AC',
-        operation: 'clear',
       },
       {
         label: '+/-',
-        operation: 'negation',
       },
       {
         label: '%',
-        operation: 'percent',
       },
       {
         label: '÷',
-        operation: 'divide',
         code: 'k',
       },
       {
         label: 7,
-        operation: 'seven',
       },
       {
         label: 8,
-        operation: 'eight',
       },
       {
         label: 9,
-        operation: 'nine',
       },
       {
         label: 'x',
-        operation: 'multiply',
         code: 'k',
       },
       {
         label: 4,
-        operation: 'four',
       },
       {
         label: 5,
-        operation: 'five',
       },
       {
         label: 6,
-        operation: 'six',
       },
       {
         label: '-',
-        operation: 'subtract',
         code: 'k',
       },
       {
         label: 1,
-        operation: 'one',
       },
       {
         label: 2,
-        operation: 'two',
       },
       {
         label: 3,
-        operation: 'three',
       },
       {
         label: '+',
-        operation: 'add',
         code: 'k',
       },
       {
         label: 0,
-        operation: 'zero',
       },
       {
         label: '.',
-        operation: 'decimal',
       },
       {
         label: '=',
-        operation: 'equals',
         code: 'k',
       },
     ];
 
+    const { total, next } = this.state;
     return (
       <div className="calculator">
         <div className="calculator__container">
+          <div className="calculator__cell">{total || next || '0'}</div>
           {calculatorOptions.map((option) => (
-            <div
-              className={`calculator__cell ${option.code ? 'operation' : ''}`}
-              key={option.operation}
-            >
-              {option.label}
-            </div>
+            <Blocks
+              key={option.label}
+              label={option.label}
+              code={option.code ? option.code : ''}
+              handleCalculate={this.handleCalculate}
+            />
           ))}
         </div>
       </div>
