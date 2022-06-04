6bb5cef62a6fd4ebcab340bd6c316ced44c432f9
Joseph Kwesi Kwentsir <kwentsirkwesi@gmail.com>
2022-06-04 04:47:59 +0000

    fix linters

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Calculator from './components/Calculator';
+import Calculator from './components/calculator';
 
 export default class App extends Component {
   constructor(props) {
