a440522e954599686250fa22839b34b54a0281fb
Joseph Kwesi Kwentsir <kwentsirkwesi@gmail.com>
2022-06-04 18:07:01 +0000

    fix linter errors

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,33 +1,33 @@
-import React, { useState } from "react";
-import Blocks from "./Blocks";
-import calculate from "../logic/calculate";
+import React, { useState } from 'react';
+import Blocks from './Blocks';
+import calculate from '../logic/calculate';
 
 const Calculator = () => {
   const [total, setTotal] = useState(null);
   const [next, setNext] = useState(null);
   const [operation, setOperation] = useState(null);
 
-  const [display, setDisplay] = useState("0");
+  const [display, setDisplay] = useState('0');
   const updateDisplay = (newObj, label) => {
-    if (label === "=" && !newObj.total) {
-      setDisplay("0");
+    if (label === '=' && !newObj.total) {
+      setDisplay('0');
       setOperation(null);
       setNext(null);
       setTotal(null);
-    } else if (label === "=" && newObj.total) {
+    } else if (label === '=' && newObj.total) {
       setDisplay(newObj.total);
       setTotal(newObj.total);
       setNext(null);
       setOperation(null);
-    } else if (label === "%" && !newObj.next && !newObj.total) {
-      setDisplay("0");
+    } else if (label === '%' && !newObj.next && !newObj.total) {
+      setDisplay('0');
       setOperation(null);
     } else if (newObj.operation === label) {
       setDisplay(`${newObj.total} ${newObj.operation}`);
     } else if (newObj.next) {
       setDisplay(newObj.next);
-    } else if (label === "AC") {
-      setDisplay("0");
+    } else if (label === 'AC') {
+      setDisplay('0');
     }
   };
 
@@ -41,66 +41,66 @@ const Calculator = () => {
 
   const calculatorOptions = [
     {
-      label: "AC",
+      label: 'AC',
     },
     {
-      label: "+/-",
+      label: '+/-',
     },
     {
-      label: "%",
+      label: '%',
     },
     {
-      label: "÷",
-      code: "k",
+      label: '÷',
+      code: 'k',
     },
     {
-      label: "7",
+      label: '7',
     },
     {
-      label: "8",
+      label: '8',
     },
     {
-      label: "9",
+      label: '9',
     },
     {
-      label: "x",
-      code: "k",
+      label: 'x',
+      code: 'k',
     },
     {
-      label: "4",
+      label: '4',
     },
     {
-      label: "5",
+      label: '5',
     },
     {
-      label: "6",
+      label: '6',
     },
     {
-      label: "-",
-      code: "k",
+      label: '-',
+      code: 'k',
     },
     {
-      label: "1",
+      label: '1',
     },
     {
-      label: "2",
+      label: '2',
     },
     {
-      label: "3",
+      label: '3',
     },
     {
-      label: "+",
-      code: "k",
+      label: '+',
+      code: 'k',
     },
     {
-      label: "0",
+      label: '0',
     },
     {
-      label: ".",
+      label: '.',
     },
     {
-      label: "=",
-      code: "k",
+      label: '=',
+      code: 'k',
     },
   ];
 
@@ -112,7 +112,7 @@ const Calculator = () => {
           <Blocks
             key={option.label}
             label={option.label}
-            code={option.code ? option.code : ""}
+            code={option.code ? option.code : ''}
             handleCalculate={handleCalculate}
           />
         ))}
